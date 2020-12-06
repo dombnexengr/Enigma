9757a90ef6dbbf17c7b220ff319f95a3e1cfb03a
DombNexen <32490783+dombnexengr@users.noreply.github.com>
2020-12-07 00:51:42 +0200

    Delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    category: "info", 
-    discription: 'discription',
-    usage: "ping",
-    execute(message, args) {
-        message.channel.send(`🏓 | Latency is: **${Date.now() - message.createdTimestamp}ms.**`);
-    },
-};
